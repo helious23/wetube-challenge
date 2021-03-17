e9504d4d375d757557864b582c36272fbd360934
Max16 <max16@naver.com>
2021-03-17 13:09:52 +0900

    #2.4 Handling Routes with Express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  console.log(req);
+  res.send("Hello");
+  console.log("Hello from home");
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
