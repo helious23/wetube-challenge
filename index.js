7f6d44684d68634529a87319e9378f5b2f7eab08
Max16 <max16@naver.com>
2021-03-17 15:31:37 +0900

    #2.7 Express Core:Middlewares part Two

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
@@ -15,12 +19,11 @@ const handleProfile = (req, res) => {
   res.send("You are on my profile");
 };
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(morgan("dev")); // logger
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
