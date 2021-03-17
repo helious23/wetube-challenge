cdd4357b28e3a983e351a84cee20be1f40169942
Max16 <max16@naver.com>
2021-03-17 15:49:21 +0900

    #2.8 Express Core:Routing

--- app.js
+++ app.js
@@ -3,29 +3,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-const PORT = 4000;
 
-const handleListening = () => {
-  console.log(`✅ Listening on: http://localhost:${PORT}`);
-};
+const handleHome = (req, res) => res.send("Hello");
 
-const handleHome = (req, res) => {
-  res.send("Hello");
-};
-
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
-};
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet({ contentSecurityPolicy: false })); // Security
 app.use(morgan("dev")); // logger
 
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
