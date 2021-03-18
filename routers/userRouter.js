a226fcc601aca2d405be0c90fbd2c89fdb2b7b06
Max16 <max16@naver.com>
2021-03-18 22:42:15 +0900

    #2.10 MVC Pattern part Two

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, (req, res) => res.send("Users"));
+userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
+userRouter.get(routes.changePassword, (req, res) =>
+  res.send("Change Password")
+);
+userRouter.get(routes.userDetail, (req, res) => res.send("User Detail")); // User Detail 이 맨 아래에 와야됨
 
 export default userRouter;
