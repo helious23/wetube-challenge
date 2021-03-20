2017f3f8909554e17dec2a94743b4116878f3f26
Max16 <max16@naver.com>
2021-03-21 08:56:58 +0900

    #2.22 Home Controller part Two

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ export const videos = [
   {
     id: 321254,
     title: "Video awesome",
-    description: "This is some awesome",
+    description: "This is something awesome",
     views: 24,
     videoFile:
-      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
+      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4",
     creator: {
       id: 12312312,
       name: "Max",
@@ -15,10 +15,10 @@ export const videos = [
   {
     id: 12323232,
     title: "Video super",
-    description: "This is some awesome",
+    description: "This is something awesome",
     views: 24,
     videoFile:
-      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
+      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4",
     creator: {
       id: 12312312,
       name: "Max",
@@ -28,10 +28,10 @@ export const videos = [
   {
     id: 1112221,
     title: "Video nice",
-    description: "This is some awesome",
+    description: "This is something awesome",
     views: 24,
     videoFile:
-      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
+      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4",
     creator: {
       id: 12312312,
       name: "Max",
@@ -41,10 +41,10 @@ export const videos = [
   {
     id: 4423123,
     title: "Video goooooodd",
-    description: "This is some awesome",
+    description: "This is something awesome",
     views: 24,
     videoFile:
-      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
+      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4",
     creator: {
       id: 12312312,
       name: "Max",
