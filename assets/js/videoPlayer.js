c41d4d59fdcd1c2e3d742c4224aa6851d16bb0e3
Max16 <max16@naver.com>
2021-04-07 23:03:48 +0900

    #9.2 Mute and Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,52 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeButton");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
+};
+
+const handleVolumeClick = () => {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+};
+
+const exitFullScreen = () => {
+  fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScrnBtn.addEventListener("click", goFullScreen);
+  document.exitFullscreen().catch((err) => Promise.resolve(err));
+  fullScrnBtn.removeEventListener("click", exitFullScreen);
+};
+
+const goFullScreen = () => {
+  videoContainer.requestFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener("click", goFullScreen);
+  fullScrnBtn.addEventListener("click", exitFullScreen);
 };
 
 const init = () => {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScrnBtn.addEventListener("click", goFullScreen);
+  document.addEventListener("fullscreenchange", () => {
+    if (!document.fullscreenElement) {
+      exitFullScreen();
+    }
+  });
 };
 
 if (videoContainer) {
